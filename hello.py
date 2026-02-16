123607bce80fa4af3bad68fa0dd7f86b6b7d64dc
Dinesh.Loganathan <dinesh14322096@gmail.com>
2026-02-16 14:14:27 +0530

    Add price calculation function

---
+++ hello.py
@@ -0,0 +1,19 @@
+dirst_nm='Alice' #This the first name
+10**2
+is_true = True
+age = 16
+can_vote = age >= 18
+
+temp=25
+
+if temp > 25:
+    print('High')
+else:
+    print('Low')
+
+def add_return(a,b)
+    return(a+b)
+    print(a+b)
+
+result = add_return(2,3)
+print(result+5)
